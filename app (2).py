05f9d14d9200dba5b2b3c417678babf59cbda93d
khadija1234305-source <khadija1234305@gmail.com>
2026-03-13 01:54:47 +0500

    Add files via upload
    Initial commit

---
+++ app (2).py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title("Khadija Rashid Portfolio")
+
+st.header("About Me")
+st.write("Hello! My name is Khadija Rashid. I am a student learning programming and making Apps and Robots .")
+
+st.header("Education")
+st.write(" Currently Doing Graduation in Embeded Systems from UOL ")
+
+st.header("Skills")
+st.write("- Robot")
+st.write("- Basic Programming")
+
+st.header("Projects")
+st.write("1. Human Follower Robot")
+st.write("2. Chatbot")
+st.write("3. Petrol Pump Control ASM")
+
+st.header("Contact")
+st.write("Email: khadija@1234305gmail.com")
